bb0129a694319f0c4e4009e16173f23fbeb3a8be
김민혁 <royalkim0707@gmail.com>
2026-04-11 17:32:35 +0900

    수정

--- src/main/java/com/weai/server/global/config/OpenApiConfig.java
+++ src/main/java/com/weai/server/global/config/OpenApiConfig.java
@@ -10,6 +10,8 @@ import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.weai.server.global.swagger.SwaggerErrorResponseCustomizer;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -17,6 +19,12 @@ public class OpenApiConfig {
 	@Bean
 	public OpenAPI weAiOpenApi() {
 		return new OpenAPI()
+
+				.servers(List.of(
+						new Server().url("http://we-ai.duckdns.org:18080").description("DuckDNS 배포 서버"),
+						new Server().url("http://localhost:8080").description("로컬 테스트 서버")
+				))
+
 			.components(new Components()
 				.addSecuritySchemes("bearerAuth", new SecurityScheme()
 					.type(SecurityScheme.Type.HTTP)
